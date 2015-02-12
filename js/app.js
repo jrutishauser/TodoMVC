615c288e71747380efdee8efd69a7bb424a07741
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-11 19:28:16 -0500

    text appended to list and also capturing input

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 	$(document).ready(function () {
 	$('#new-todo').on('keypress', function(e) {
 		if(e.keyCode == 13) {
+			var newTask = $('#new-todo').val();	
+			console.log(newTask);
 			$('#todo-list').append('Banana');	
 		}
 	});		
