7cd8ab7bb8cc149ec0842603c26ef217eb3dbe37
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-11 19:42:31 -0500

    new LI appended to list with every press of the enter key

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@
 		if(e.keyCode == 13) {
 			var newTask = $('#new-todo').val();	
 			console.log(newTask);
-			$('#todo-list').append('Banana');	
+			$('#todo-list').append('<li><div class="view"><input class="toggle" type="checkbox">' +
+				'<label>Buy a unicorn</label><button class="destroy"></button></div>' +
+				'<input class="edit" value="Rule the web"></li>');	
 		}
 	});		
 	
