e4204565b20c696cd913ac62509b42885c13fecf
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-12 00:07:53 -0500

    checked off items are marked completed

--- js/app.js
+++ js/app.js
@@ -4,17 +4,25 @@
 	$('#new-todo').on('keypress', function(e) {
 		if(e.keyCode == 13) {
 			var newTask = $('#new-todo').val();	
-			console.log(newTask);
 			$('#todo-list').append('<li><div class="view"><input class="toggle" type="checkbox">' +
 				'<label>' + newTask + '</label><button class="destroy"></button></div>' +
 				'<input class="edit" value="' + newTask + '"></li>');	
+			$('#new-todo').val('');	
 		}
 	});		
 	
+	$('#todo-list').on('click', '.toggle', function(e){
+		$(this).closest('li').toggleClass('completed');		               
+     });	
 	
-	
-	
-	
+//	$('#toggle-all').on('click', function(e){
+	//	$('#todo-list > li').removeClass('completed');
+	//	$('#todo-list > li').toggleClass('completed');
+	//	$('.toggle').toggleattr('checked', true);	
+//	});	
+	$('#todo-list').on('dblclk', 'li', function(e){
+		$(this).closest('.edit').removeClass('hidden');
+	});	
 	
 	
 	
